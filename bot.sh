b3b9407ac3bfb8f6d02e376823ca071c94305ba1
sokhorn <sokhornkhy88@gmail.com>
2022-06-23 08:43:53 +0700

    Code have been commit from terminal

--- bot.sh
+++ bot.sh
@@ -4,7 +4,7 @@ token='5476532482:AAFBrQgyMQXDN6bc27nuKYBfoYyAoz636bI'
 tele_url="https://api.telegram.org/bot${token}";
 # curl -s "${tele_url}/getUpdates" | json_pp
 
-chat_id=$(curl -s "${tele_url}/getUpdates" | jq -r ".result[-1].update_id")
+# chat_id=$(curl -s "${tele_url}/getUpdates" | jq -r ".result[-1].update_id")
 echo $chat_id
 # curl -s "${tele_url}/sendMessage?chat_id=${chat_id}" \
 #   --data-urlencode "text=$1" | json_pp
